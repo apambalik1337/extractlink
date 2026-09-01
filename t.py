1107bcf46d963256ca7f260944322a5d508e00f2
kasihfauna <g-ipgp252100152@moe-dl.edu.my>
2026-09-01 18:04:04 +0800

    Update t.py

--- t.py
+++ t.py
@@ -1,16 +1,7 @@
 import re
 import os
 
-banner="""
- \033[94m __ _ _ __ ___ (_)_ __ \033[91m ___(_) | ___ __ _   _ _   _
-\033[94m / _` | '_ ` _ \| | '_  \033[91m|_  / | |/ / '__| | | | | | |
-\033[94m| (_| | | | | | | | | | \033[91m|/ /| |   <| |  | |_| | |_| |
-\033[94m \__,_|_| |_| |_|_|_| |_\033[91m/___|_|_|\_\_|   \__, |\__, |
- \033[94m                       \033[91m                |___/ |___/
- \033[0m                                                """
-print(banner)
-
-print("Example: /home/aminzikryy/Desktop/text.txt")
+print("Example: /home/apambalik/Desktop/text.txt")
 file_path = input("Please enter the path of the input text file (.txt): ").strip('\"')
 
 with open(file_path, "r") as file:
